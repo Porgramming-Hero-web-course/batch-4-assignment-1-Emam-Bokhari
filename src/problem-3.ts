22d8d2b3beb2116dbba5e053a6cfd05109c5b4de
Moshfiqur Rahman <studentemam@gmail.com>
2024-11-06 21:52:46 +0600

    feat: add countWordOccurrences function for cout word and return number of count

---
+++ src/problem-3.ts
@@ -0,0 +1,21 @@
+{
+    const countWordOccurrences = (sentence: string, word: string): number => {
+        
+        const convertLowercaseSentence = sentence.toLowerCase();
+        const convertLowercaseWord = word.toLowerCase();
+     
+        const replaceRegxInSentence = convertLowercaseSentence.replace(/[.,!?]/g, "");
+    
+        const splitSentence = replaceRegxInSentence.split(" ");
+    
+        const result = splitSentence.filter(item => item === convertLowercaseWord);
+    
+        return result.length;
+    };
+    
+    const result = countWordOccurrences("TypeScript is great. I love TypeScript!", "typescript");
+    console.log(result); 
+    
+    
+    
+}
