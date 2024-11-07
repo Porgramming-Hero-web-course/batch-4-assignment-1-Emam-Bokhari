38c45f1f9e906d79ce2273ebdda67fd751b12903
Moshfiqur Rahman <studentemam@gmail.com>
2024-11-07 07:35:52 +0600

    feat: addd Car class and create getCarAge method for calculate car age

---
+++ dist/problem-7.js
@@ -0,0 +1,17 @@
+"use strict";
+{
+    class Car {
+        constructor(make, model, year) {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+        getCarAge() {
+            const currentYear = new Date().getFullYear();
+            return currentYear - this.year;
+        }
+    }
+    const car = new Car("Honda", "Civic", 2018);
+    const result = car.getCarAge();
+    console.log(result);
+}
